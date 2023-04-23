98429e1912bc7cb74a33acd1bcb5593d51c15f4d
Adz 1DoubleD <adz1doubled@gmail.com>
2023-04-23 19:53:07 +0100

    feat: added loanOriginated tracking

--- scanner.py
+++ scanner.py
@@ -8,7 +8,18 @@ import ca
 infura_url = f'https://mainnet.infura.io/v3/{keys.infura}'
 web3 = Web3(Web3.HTTPProvider(infura_url))
 
-xchange = web3.eth.contract(address=ca.factory, abi=api.get_abi(ca.uniswap))
+factory = web3.eth.contract(address=ca.factory, abi=api.get_abi(ca.uniswap))
+ill001 = web3.eth.contract(address=ca.ill001, abi=api.get_abi(ca.ill001))
+ill002 = web3.eth.contract(address=ca.ill002, abi=api.get_abi(ca.ill002))
+ill003 = web3.eth.contract(address=ca.ill003, abi=api.get_abi(ca.ill003))
+
+async def new_loan(event):
+    print(event)
+    await application.bot.send_photo(
+        "-1001780235511",
+        photo=open('media/logo10.png', 'rb'),
+        caption=f'*New Loan Originated*\n\n{event["loanID"]}\n\n'
+                f'https://etherscan.io/tx/{event["transactionHash"].hex()}', parse_mode='Markdown')
 
 async def new_pair(event):
     name_token0 = api.get_token_name(event["args"]["token0"])
@@ -21,22 +32,27 @@ async def new_pair(event):
                 f'Token 1: {name_token1}\n\n'
                 f'https://etherscan.io/tx/{event["transactionHash"].hex()}', parse_mode='Markdown')
 
-
-async def log_loop(event_filter, poll_interval):
+async def log_loop(pair_filter, ill001_filter, ill002_filter, ill003_filter, poll_interval):
     while True:
-        for PairCreated in event_filter.get_new_entries():
+        for PairCreated in pair_filter.get_new_entries():
             await new_pair(PairCreated)
+        await asyncio.sleep(poll_interval)
+        for LoanOriginated in \
+                ill001_filter.get_new_entries() or ill002_filter.get_new_entries() or ill003_filter.get_new_entries():
+            await new_loan(LoanOriginated)
         await asyncio.sleep(poll_interval)
 
 
 def main():
-    print("Scanning X7 Finance Ecosystem...")
-    pair_filter = xchange.events.PairCreated.create_filter(fromBlock='latest')
+    print("Scanning X7 Finance ecosystem")
+    pair_filter = factory.events.PairCreated.create_filter(fromBlock='latest')
+    ill001_filter = ill001.events.LoanOriginated.create_filter(fromBlock='latest')
+    ill002_filter = ill002.events.LoanOriginated.create_filter(fromBlock='latest')
+    ill003_filter = ill003.events.LoanOriginated.create_filter(fromBlock='latest')
     loop = asyncio.new_event_loop()
     asyncio.set_event_loop(loop)
     try:
-        loop.run_until_complete(
-            asyncio.gather(log_loop(pair_filter, 2)))
+        loop.run_until_complete(asyncio.gather(log_loop(pair_filter, ill001_filter, ill002_filter, ill003_filter, 2)))
     finally:
         loop.close()
 
