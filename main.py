be6cbdb632a75336fe9cb708ff83e2a82ac1063f
Adz1DoubleD <adz1doubled@gmail.com>
2023-07-16 15:05:46 +0100

    feat: add xchange pair info command

--- main.py
+++ main.py
@@ -143,7 +143,7 @@ if __name__ == "__main__":
     application.add_handler(CommandHandler(["nft", "nfts"], commands.nft))
     application.add_handler(CommandHandler(["on_chain", "onchain", "message"], commands.on_chain))
     application.add_handler(CommandHandler(["opensea", "os"], commands.opensea))
-    application.add_handler(CommandHandler("pair", commands.pair))
+    application.add_handler(CommandHandler(["pair", "pairs"], commands.pair))
     application.add_handler(CommandHandler("pioneer", commands.pioneer))
     application.add_handler(CommandHandler("proposal", commands.proposal))
     application.add_handler(CommandHandler(["pool", "lpool", "lendingpool"], commands.pool))
