e66300b3342d2cb32d0fd009d044b11f5a8c43db
Kirill Volozhanin <volozhaninkirill@gmail.com>
2023-07-24 14:03:33 +0500

    Refactor api/dune.py
    * Optimize all functions
    * Rename 'ID' param in make_api_url() to 'identifier'. Params should be lowercase, but 'id' is a built-in name. So this is the solution.

--- api/dune.py
+++ api/dune.py
@@ -10,34 +10,35 @@ HEADER = {"x-dune-api-key": API_KEY}
 BASE_URL = "https://api.dune.com/api/v1/"
 
 
-def make_api_url(module, action, ID):
-    url = BASE_URL + module + "/" + ID + "/" + action
-    return url
+def make_api_url(module, action, identifier):
+    return f"{BASE_URL}{module}/{identifier}/{action}"
 
 
 def execute_query(query_id, engine="small"):
-    url = make_api_url("query", "execute", query_id)
-    params = {
-        "performance": engine,
-    }
-    response = post(url, headers=HEADER, params=params)
-    execution_id = response.json()['execution_id']
-    return execution_id
+    response = post(
+        make_api_url("query", "execute", query_id),
+        headers=HEADER,
+        params={"performance": engine}
+    )
+    return response.json()['execution_id']
 
 
 def get_query_status(execution_id):
-    url = make_api_url("execution", "status", execution_id)
-    response = get(url, headers=HEADER)
-    return response
+    return get(
+        make_api_url("execution", "status", execution_id),
+        headers=HEADER
+    )
 
 
 def get_query_results(execution_id):
-    url = make_api_url("execution", "results", execution_id)
-    response = get(url, headers=HEADER)
-    return response
+    return get(
+        make_api_url("execution", "results", execution_id),
+        headers=HEADER
+    )
 
 
 def cancel_query_execution(execution_id):
-    url = make_api_url("execution", "cancel", execution_id)
-    response = get(url, headers=HEADER)
-    return response
+    return get(
+        make_api_url("execution", "cancel", execution_id),
+        headers=HEADER
+    )
